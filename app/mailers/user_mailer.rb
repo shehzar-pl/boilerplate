ecb4b417fd25b4e590001ccb1aadef9a1a2b2450
Hamza-PL <muhammad.hamzaarshad@purelogics.net>
2023-03-24 17:29:42 +0500

    Added otp generation through email and its verification

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < Devise::Mailer
     @token = token
     mail(to: @user.email, subject: 'Reset your password')
   end
+
+  def send_otp(user)
+    @user = user
+    mail(to: user.email, subject: "Your OTP")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: { passwords: 'users/passwords' }
+  devise_for :users, controllers: { passwords: 'users/passwords', sessions: 'users/sessions' }
+  resources :otps, only: [:new]
+  post 'generate_otp', to: 'otps#create'
+  get 'verify_otp', to: 'otps#verify'
+  post 'verify_otp', to: 'otps#check'
   
   root to: "home#index"
 end

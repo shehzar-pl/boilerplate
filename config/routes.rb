6fd79d3a19546c7f4e6d8e04682a33b34acd1a1e
Shazar <muhammad.shazar@purelogics.net>
2023-03-24 14:23:15 +0500

    Fixed password resetting bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: { passwords: 'users/passwords' }
+  devise_for :users
   
   root to: "home#index"
 end

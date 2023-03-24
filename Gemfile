ffb44dbcb2da187dc546e4b30e9a560f6dc86561
Shazar <muhammad.shazar@purelogics.net>
2023-03-24 16:11:10 +0500

    Authentication using Google

--- Gemfile
+++ Gemfile
@@ -35,6 +35,12 @@ gem 'sass-rails'
 
 gem 'byebug'
 
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
+
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable,
+        :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_google(email:, full_name:, uid:, avatar_url:, provider:)
+    create_with(uid: uid, provider: provider).find_or_create_by!(email: email)
+  end
+
+  def password_required?
+    # If the user is signing in with Google, bypass the password validation
+    return false if provider == "google"
+    # Otherwise, perform the default password validation
+    super
+  end
 end

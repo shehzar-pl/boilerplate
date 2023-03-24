74a55cb889f5ab1202afff6ca1bce5760aea1773
Shazar <muhammad.shazar@purelogics.net>
2023-03-24 16:44:26 +0500

    Added Facebook Authentication

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'byebug'
 gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem 'omniauth-rails_csrf_protection'
+gem 'omniauth-facebook'
 
 gem 'dotenv-rails', groups: [:development, :test]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable,
-        :omniauthable, omniauth_providers: [:google_oauth2]
+        :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
 
   def self.from_google(email:, full_name:, uid:, avatar_url:, provider:)
     create_with(uid: uid, provider: provider).find_or_create_by!(email: email)
@@ -14,5 +14,13 @@ class User < ApplicationRecord
     return false if provider == "google"
     # Otherwise, perform the default password validation
     super
+  end
+
+  def self.from_omniauth(auth)
+    name_split = auth.info.name.split(" ")
+    user = User.where(email: auth.info.email).first
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0, 20])
+
+    user
   end
 end
